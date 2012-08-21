f7ee6c5067a731a7eed575332ceef963d040e5ab
Robert Sedlacek <rs@474.at>
2012-08-21 19:55:37 +0200

    removed no longer needed mkpath option

--- lib/App/PMark/Command/Export.pm
+++ lib/App/PMark/Command/Export.pm
@@ -9,10 +9,6 @@ sub abstract { 'export your profile data' }
 
 sub usage_desc { '%c export %o <target>' }
 
-sub opt_spec {
-    ['mkpath|p', q!try to create the directory if it doesn't exist!],
-}
-
 sub validate_args {
     my ($self, $option, $args) = @_;
     $self->usage_error('Missing an export target argument')
@@ -29,9 +25,6 @@ sub description {
         The produced JSON file can be subscribed to by anyone who has
         supported access to the file. See the 'subscribe' command for more
         information about this.
-
-        If you pass the '--mkpath' option the directory of the file will
-        be created if it doesn't exist yet.
     };
 }
 
@@ -44,7 +37,6 @@ sub examples {
 sub execute {
     my ($self, $profile, $option, $target) = @_;
     make_file($target)->put($profile->file->get);
-    print "\n";
     return 1;
 }
 
